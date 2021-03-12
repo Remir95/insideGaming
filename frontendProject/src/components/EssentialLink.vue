7ed97d29900f95721f159f90227af670637efd57
Remi Ringard <ringard_remi@yahoo.fr>
2021-03-12 10:56:02 +0100

    Fist Commit init

---
+++ frontendProject/src/components/EssentialLink.vue
@@ -0,0 +1,46 @@
+<template>
+  <q-item clickable tag="a" target="_blank" :href="link">
+    <q-item-section v-if="icon" avatar>
+      <q-icon :name="icon" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label class="hello">{{ title }}</q-item-label>
+      <q-item-label caption>
+        {{ caption }}
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: "EssentialLink",
+  props: {
+    title: {
+      type: String,
+      required: false,
+    },
+
+    caption: {
+      type: String,
+      default: "",
+    },
+
+    link: {
+      type: String,
+      default: "#",
+    },
+
+    icon: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.hello {
+  font-size: 2.2em;
+  border: 1.8px solid red;
+}
+</style>
---
+++ frontendProject/src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-input
+      v-model="name"
+      label="name"
+      outlined
+      standout="bg-blue-8 text-white"
+      class="q-ma-md"
+    >
+      <template v-slot:append>
+        <q-icon name="person"></q-icon>
+      </template>
+    </q-input>
+    <q-btn color="red" round icon="flag" size="lg" :loading="false"> </q-btn>
+  </q-page>
+</template>
+
+<script>
+  export default {
+    name: 'PageIndex', 
+    components:{
+
+    }
+  }
+  data() {
+    return{
+      name:'JOHN'
+    }
+  }
+</script>
